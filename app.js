d14e840c3bdbb26a61a3fa709865a80ca6fd93b0
Sameer Hussain <64489092+Sameersheikh147@users.noreply.github.com>
2024-02-21 23:50:48 +0500

    all fiels uplaod

---
+++ app.js
@@ -0,0 +1,169 @@
+
+// chapter no 1 
+
+// Q no 1 :
+
+
+alert("error!! Please Enter a valid Password");
+
+// Q no 2:
+
+
+alert("Welcome to js Land \n Happy Coding");
+
+
+
+// Q no 3:
+
+alert(" Welcome to js land");
+
+
+// Q no 4:
+
+
+
+alert(" Happy Coding \n Prevent this page from creating additional dialogs");
+
+// Q no 4:
+
+alert(" hello... I Can run Js through my web browser console");
+
+
+
+
+// Chapter No 2 start
+
+
+// var userName = "Sameer"
+// var myName  = " Hussain"
+// var age = 21;
+// var course ="Certified Jawan pakistan course"
+
+
+// alert(userName);
+// alert(myName);
+// alert(age);
+// alert(course);
+
+
+
+var text1 = "Pizza"
+var text2 = "Pizz"
+var text3 = "Piz"
+var text4 = "Pi"
+var text5 = "P"
+
+alert (text1  +"\n" + text2 +"\n" + text3  +"\n" + text4  +"\n" + text5)
+
+
+
+var emailAdrress = "smrsheikh147@gmail.com";
+
+alert ("My Email Address is " + emailAdrress)
+
+
+var book = "A smarter way to learn JavaScript"
+alert (book)
+
+
+var message = ("yah ! I can write an html through Js")
+
+document.write(message)
+
+
+var picture = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert (picture);
+
+
+// Chapter no 3 
+
+// Q no 1
+
+
+var age = 18;
+
+alert("I am " + age + " years old")
+
+
+// Q no 2
+
+var track = 14; 
+
+alert("you have visited site " + track + " times");
+
+
+// Q no 3
+
+var n  = 1990 ;
+var messageString = "my birth year is";
+document.write( "<br/>", messageString + n);
+
+
+// Q no 4
+
+
+var visitorName =  "John doe";
+var productTitle =  "ordered" + 5;
+var quantity =  "T-shirt (s)" 
+
+document.write = (  "b" ,visitorName ,  "b" , productTitle , quantity , "on XYZ clothing Store");
+
+
+
+var firstName = "Sameer";
+var lastName = "hussain";
+var email = "email@gmail.com";
+var phone = 30123456789;
+var pwrd = "12345fiif"
+var alldata = (firstName , lastName ,email, phone , pwrd );
+alert(alldata , "this is all data alert box");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var number =20;
+// var number2 = 30;
+// const number3 = 100;
+
+// var allNumber = number + number2 + number3;
+
+// allNumber = 250 ;
+
+// console.log(allNumber)
+
+// console.log(allNumber)
+
+
+ 
+
+// var myElement = document.getElementById("demo");
+// myElement.style.color = "red";
